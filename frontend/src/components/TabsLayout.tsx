e0ba184f88dc6e56ab7180a20168f155b84c6e81
Attila Toth <hello@attilatoth.dev>
2025-02-22 19:10:46 +0100

    add demo selector popup

--- frontend/src/components/TabsLayout.tsx
+++ frontend/src/components/TabsLayout.tsx
@@ -1,5 +1,5 @@
-import type { ReactElement } from 'react';
-import { Tabs, Tab, Stack } from 'react-bootstrap';
+import { useState, type ReactElement } from 'react';
+import { Tabs, Tab, Stack, Button, Modal, Form, Row, Col } from 'react-bootstrap';
 import type { IconType } from 'react-icons';
 import { FaGaugeSimple, FaRocket, FaCircleInfo } from 'react-icons/fa6';
 import { Dashboard } from '@/components/Dashboard';
@@ -16,13 +16,13 @@ interface SectionTab {
 }
 
 const tabs: readonly SectionTab[] = [
- // {
-   // key: 'dashboard',
-   // title: 'Dashboard',
-   // Icon: FaGaugeSimple,
-   // Component: Dashboard,
-  //},
   {
+    key: 'dashboard',
+    title: 'Terraform settings',
+    Icon: FaGaugeSimple,
+    Component: Dashboard,
+  },
+  /*{
     key: 'scenarios',
     title: 'Scenarios',
     Icon: FaRocket,
@@ -33,31 +33,67 @@ const tabs: readonly SectionTab[] = [
     title: 'About',
     Icon: FaCircleInfo,
     Component: About,
-  },
+  },*/
 ];
 
-export const TabsLayout = (): ReactElement => (
-  <Tabs
-    defaultActiveKey="scenarios"
-    id="controlTabs"
-    className="nav-tabs nav-fill"
-  >
-    {tabs.map(({ key, title, Icon, Component }) => (
-      <Tab
-        key={key}
-        eventKey={key}
-        title={
-          <TabHeader
-            title={title}
-            Icon={Icon}
-          />
-        }
-      >
-        <Component />
-      </Tab>
-    ))}
-  </Tabs>
-);
+export const TabsLayout = (): ReactElement => {
+  const [showPopup, setShowPopup] = useState(true);
+  const [selectedDemo, setSelectedDemo] = useState('scylladb-cloud');
+  const handleInit = async () => {
+    setShowPopup(false);
+    try {
+
+      const response = await fetch('http://localhost:5000/choose-demo', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ demo: selectedDemo }),
+      });
+      
+      if (response.ok) {
+        console.log("`terraform init` started")
+      } else {
+        const errorText = await response.text();
+        console.error('Failed to submit demo selection:', errorText);
+      }
+    } catch (error) {
+      console.error('Error submitting demo selection:', error);
+    }
+  };
+
+  return (
+    <>
+      <Modal show={showPopup} onHide={() => setShowPopup(false)} backdrop="static" keyboard={false}>
+        <Modal.Header>
+          <Modal.Title>Choose a ScyllaDB performance DEMO</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formDropdown">
+              <Form.Select value={selectedDemo} onChange={(e) => setSelectedDemo(e.target.value)}>
+                <option value="scylladb-cloud">ScyllaDB Cloud</option>
+                <option value="scylladb-enterprise">ScyllaDB Enterprise</option>
+                <option value="tablets-scaling">ScyllaDB on AWS - Scaling 3 to 6 nodes</option>
+              </Form.Select>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleInit}>
+            Initialize
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Tabs defaultActiveKey="dashboard" id="controlTabs" className="nav-tabs nav-fill">
+        {tabs.map(({ key, title, Icon, Component }) => (
+          <Tab key={key} eventKey={key} title={<TabHeader title={title} Icon={Icon} />}>
+            <Component />
+          </Tab>
+        ))}
+      </Tabs>
+    </>
+  );
+};
 
 interface TabHeaderProps {
   readonly title: string;
