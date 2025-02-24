b7fdc4d99ae458dbedafc31517aed1008b32881e
Attila Toth <hello@attilatoth.dev>
2025-02-24 20:33:23 +0100

    app.py minor refactor

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, abort, request, jsonify
+from flask import Flask, send_from_directory, abort, request
 from flask_socketio import SocketIO
 from flask_cors import CORS
 import os
@@ -30,11 +30,14 @@ def read_selected_demo():
     with open(demo_config_file, "r") as json_file:
         return json.load(json_file)["demo"]
 
+def ansible_folder_path():
+    return os.path.join(program_cwd, read_selected_demo(), "ansible")
+
 def create_tf_vars_file(variables, output_folder):
     with open(f"{output_folder}/{user_tf_variables_file}", 'w') as file:
         json.dump(variables, file, indent=4) 
 
-def clean_console_output(line):
+def console_output(line):
     ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
     clean_line = ansi_escape.sub('', line)
     socketio.emit("playbook_output", {"output": clean_line.strip()+"\n"})
@@ -54,7 +57,6 @@ def choose_demo():
     chosen_demo = request.json['demo']
     with open(demo_config_file, "w") as json_file:
         json.dump({"demo": chosen_demo}, json_file, indent=4)
-    ansible_folder = os.path.join(program_cwd, f"{chosen_demo}/ansible")
     tf_folder = os.path.join(program_cwd, chosen_demo)
         
     # terraform init
@@ -63,9 +65,9 @@ def choose_demo():
     cmd = ["terraform", "init"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, cwd=tf_folder)
     for line in process.stdout:
-        clean_console_output(line)
+        console_output(line)
         socketio.sleep(0)
-    clean_console_output("\nTerraform init complete!")
+    console_output("\nTerraform init complete!")
     process.wait()
     return "Demo selected and Terraform initialized", 200
 
@@ -74,17 +76,15 @@ def choose_demo():
 def terraform_plan():
     chosen_demo = read_selected_demo()
     tf_folder = os.path.join(program_cwd, chosen_demo)
-    print(tf_folder)
     cmd = ["terraform", "plan", f"-var-file={user_tf_variables_file}"]
 
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, cwd=tf_folder)
     
     for line in process.stdout:
-        clean_console_output(line)
+        console_output(line)
         socketio.sleep(0)
-    
-    clean_console_output("\nTerraform plan complete!")
     process.wait()
+    console_output("\nTerraform plan complete!")
     return "Terraform plan finished", 200
 
 
@@ -99,12 +99,10 @@ def calculate_read_write_ratio(reads, writes):
 @app.route("/tf-apply", methods=["POST"])
 def terraform_apply():
     tf_vars = request.json
-    chosen_demo = ""
-    # read chosen demo
     chosen_demo = read_selected_demo()
    
     tf_folder = os.path.join(program_cwd, chosen_demo)
-    # read and write  TF variables
+    # read and write TF variables
     with open(f"{tf_folder}/{user_tf_variables_file}", "r") as json_file:
         variables = json.load(json_file) 
         
@@ -125,10 +123,10 @@ def terraform_apply():
     
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, cwd=tf_folder)
     for line in process.stdout:
-        clean_console_output(line)
+        console_output(line)
         socketio.sleep(0)
-    clean_console_output("\nTerraform apply complete!")
     process.wait()
+    console_output("\nTerraform apply complete!")
     return "Terraform apply finished", 200
     
 @app.route("/tf-destroy", methods=["GET"])
@@ -139,10 +137,10 @@ def terraform_destroy():
 
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, cwd=tf_folder)
     for line in process.stdout:
-        clean_console_output(line)
+        console_output(line)
         socketio.sleep(0)
     process.wait()
-    clean_console_output("\nTerraform destroy complete!")
+    console_output("\nTerraform destroy complete!")
     return "Terraform destroy finished", 200
 
 @app.route("/data/<path:filename>")
@@ -160,35 +158,39 @@ globals()["process"] = None
 
 def run_ansible_playbook(playbook_path):
     playbook_cmd = ["ansible-playbook", playbook_path]
-    process = subprocess.Popen(playbook_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, cwd=ansible_folder)
+    process = subprocess.Popen(playbook_cmd,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               text=True,
+                               cwd=ansible_folder_path())
     for line in process.stdout:
-        socketio.emit("playbook_output", {"output": line})
+        console_output(line)
         socketio.sleep(0)
     process.wait()
 
 @socketio.on("original_cluster")
 def handle_original_cluster():
-    run_ansible_playbook(ansible_folder + "/1_original_cluster.yml")
+    run_ansible_playbook("1_original_cluster.yml")
 
 @socketio.on("sample_data")
 def handle_sample_data():
-    run_ansible_playbook(ansible_folder + "/2_restore_snapshot.yml")
+    run_ansible_playbook("2_restore_snapshot.yml")
 
 @socketio.on("start_stress")
 def handle_start_stress():
-    run_ansible_playbook(ansible_folder + "/3_stress.yml")
+    run_ansible_playbook("3_stress.yml")
 
 @socketio.on("scale_out")
 def handle_scale_out():
-    run_ansible_playbook(ansible_folder + "/4_scale_out.yml")
+    run_ansible_playbook("4_scale_out.yml")
 
 @socketio.on("stop_stress")
 def handle_stop_stress():
-    run_ansible_playbook(ansible_folder + "/5_stop_stress.yml")
+    run_ansible_playbook("5_stop_stress.yml")
 
 @socketio.on("scale_in")
 def handle_scale_in():
-    run_ansible_playbook(ansible_folder + "/6_scale_in.yml")
+    run_ansible_playbook("6_scale_in.yml")
 
 
 def parse_ansible_inventory(inventory_file):
