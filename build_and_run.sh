c85be03d10e1c1e8a64de4848a29b2d00a6b5936
Attila Toth <hello@attilatoth.dev>
2025-02-26 01:17:22 +0100

    add build and run script

---
+++ build_and_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the config file exists
+CONFIG_FILE="config.json"
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: Config file '$CONFIG_FILE' not found!"
+    exit 1
+fi
+
+
+# Read JSON values
+extract_value() {
+    grep -oP '"'"$1"'"\s*:\s*"\K[^"]+' "$CONFIG_FILE"
+}
+aws_creds_file=$(extract_value "aws_creds_file")
+aws_creds_profile=$(extract_value "aws_creds_profile")
+region=$(extract_value "region")
+scylla_cloud_token=$(extract_value "scylla_cloud_token")
+
+# Build image
+docker build -t scylla-demo .
+
+# Run the container
+docker run -p 5000:5000 --name scylla-demo --rm -v $aws_creds_file:/app/.aws/credentials -d scylla-demo 
