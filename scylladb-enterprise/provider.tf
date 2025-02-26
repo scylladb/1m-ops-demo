7e1fc2f89368b355c560f3dc784ac019994a22b9
Attila Toth <hello@attilatoth.dev>
2025-02-26 01:15:22 +0100

    fix aws auth for docker

--- scylladb-enterprise/provider.tf
+++ scylladb-enterprise/provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region                   = var.region
-  shared_credentials_files = ["${var.aws_creds_file}"]
-  profile                  = var.aws_creds_profile
+  shared_credentials_files = ["/app/.aws/credentials"]
+  profile = var.aws_creds_profile
+  
 }
