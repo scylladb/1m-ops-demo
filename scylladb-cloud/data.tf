f87e4c4d6d7f849cad413f0da73ab4df7fcb9273
Attila Toth <hello@attilatoth.dev>
2025-03-04 17:07:57 +0100

    ami lookup fix

--- scylladb-cloud/data.tf
+++ scylladb-cloud/data.tf
@@ -7,3 +7,23 @@ data "aws_region" "current" {}
 data "scylladbcloud_cql_auth" "scylla" {
   cluster_id = scylladbcloud_cluster.scylladbcloud.id
 }
+
+data "aws_ami" "loader_ami" {
+  most_recent      = true
+  owners           = ["158855661827"]
+
+  filter {
+    name   = "name"
+    values = [var.loader_ami_name]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
